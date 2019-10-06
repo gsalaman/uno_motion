0ddcb1ebef3a46827f6ec0292dfa837cf049fa1b
gsalaman <43499190+gsalaman@users.noreply.github.com>
2019-10-06 08:41:14 -0600

    Initial commit

---
+++ uno_motion.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+const int ANALOG_PIN = A0; 
+#define SERVO_PIN 12
+
+Servo my_servo;
+
+void setup() 
+{
+  Serial.begin(9600);
+  my_servo.attach(SERVO_PIN);
+}
+
+void loop() 
+{
+  int pot_value;
+  int servo_val;
+
+  pot_value = analogRead(ANALOG_PIN);
+
+  servo_val = map(pot_value, 0, 1023, 0, 180);
+
+  my_servo.write(servo_val);
+
+  delay(1);
+
+}
